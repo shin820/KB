0fcf6120ab6a1e5d7fc850ae2d953f0831c95122
shin820 <shin820@163.com>
2017-05-17 15:23:58 +0800

    add log

--- KB.WebApi/Global.asax.cs
+++ KB.WebApi/Global.asax.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using KB.WebApi.Core;
+using log4net;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,8 +14,12 @@ namespace KB.WebApi
 {
     public class WebApiApplication : System.Web.HttpApplication
     {
+        private static readonly ILog log = LogManager.GetLogger(typeof(HttpApplication));
+
         protected void Application_Start()
         {
+            log.Info("Application starting...");
+
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
@@ -28,6 +33,42 @@ namespace KB.WebApi
                     "KB.Dto"
                 })
             );
+
+            log.Info("Application started...");
+        }
+
+        protected void Application_Error()
+        {
+            var raisedException = Server.GetLastError();
+            if (raisedException != null)
+            {
+                log.Error(raisedException.Message, raisedException);
+            }
+
+            //HttpContext httpContext = HttpContext.Current;
+            //if (httpContext != null)
+            //{
+            //    RequestContext requestContext = ((MvcHandler)httpContext.CurrentHandler).RequestContext;
+
+            //    if (requestContext.HttpContext.Request.IsAjaxRequest())
+            //    {
+            //        httpContext.Response.Clear();
+            //        string controllerName = requestContext.RouteData.GetRequiredString("controller");
+            //        IControllerFactory factory = ControllerBuilder.Current.GetControllerFactory();
+            //        IController controller = factory.CreateController(requestContext, controllerName);
+            //        ControllerContext controllerContext = new ControllerContext(requestContext, (ControllerBase)controller);
+
+            //        JsonResult jsonResult = new JsonResult();
+            //        jsonResult.Data = new { success = false, serverError = "500" };
+            //        jsonResult.JsonRequestBehavior = JsonRequestBehavior.AllowGet;
+            //        jsonResult.ExecuteResult(controllerContext);
+            //        httpContext.Response.End();
+            //    }
+            //    else
+            //    {
+            //        httpContext.Response.Redirect("~/Error");
+            //    }
+            //}
         }
     }
 }
