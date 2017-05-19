6983c025de781a34d58c9838f34fa108b18ab55f
shin820 <shin820@163.com>
2017-05-19 15:46:43 +0800

    update project structure.

--- KB.WebApi/Global.asax.cs
+++ KB.WebApi/Global.asax.cs
@@ -30,7 +30,7 @@ namespace KB.WebApi
 
             Mapper.Initialize(cfg =>
                 cfg.AddProfiles(new[] {
-                    "KB.ApplicationService"
+                    "KB.Application"
                 })
             );
 
