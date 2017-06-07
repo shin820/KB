670abb63838c4b4c2bc50787fd259f5b5a82943e
shin <shin820@163.com>
2017-06-07 16:14:52 +0800

    update

--- KB.WebApi/Controllers/ValuesController.cs
+++ KB.WebApi/Controllers/ValuesController.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using Mustache;
+using Nustache.Core;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading;
 using System.Web;
 using System.Web.Http;
@@ -18,7 +22,7 @@ namespace KB.WebApi.Controllers
             if (test == null)
             {
                 HttpContext.Current.Session["test"] = "test";
-                return new string[] {};
+                return new string[] { };
             }
             else
             {
@@ -47,6 +51,95 @@ namespace KB.WebApi.Controllers
         // DELETE api/values/5
         public void Delete(int id)
         {
+        }
+
+
+        [Route("nustache")]
+        public HttpResponseMessage GetNustachePage()
+        {
+            string templatePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "article.html");
+
+            using (StreamReader reader = new StreamReader(templatePath))
+            {
+                string strTemp = reader.ReadToEnd();
+
+                var data = Template.Create();
+                string result = Render.StringToString(strTemp, data);
+
+                var response = new HttpResponseMessage();
+                response.Content = new StringContent(result);
+                response.Content.Headers.ContentType = new MediaTypeHeaderValue("text/html");
+
+                return response;
+            }
+        }
+
+        [Route("mustache-sharp")]
+        public HttpResponseMessage GetMustacheSharpPage()
+        {
+            string templatePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "article2.html");
+
+            using (StreamReader reader = new StreamReader(templatePath))
+            {
+                string strTemp = reader.ReadToEnd();
+
+                var data = Template.Create();
+
+                FormatCompiler compiler = new FormatCompiler();
+                Generator generator = compiler.Compile(strTemp);
+                string result = generator.Render(data);
+
+                var response = new HttpResponseMessage();
+                response.Content = new StringContent(result);
+                response.Content.Headers.ContentType = new MediaTypeHeaderValue("text/html");
+
+                return response;
+            }
+        }
+
+        public class Template
+        {
+            public string Title { get; set; }
+            public string Breadcrumbs { get; set; }
+            public string Body { get; set; }
+            public bool IfAllowFeedback { get; set; }
+            public int HelfulPercent { get; set; }
+            public List<Item> Items { get; set; }
+            public Complex Comp { get; set; }
+
+            public static Template Create()
+            {
+                return new Template
+                {
+                    Title = "Test mustache",
+                    Breadcrumbs = "Test",
+                    Body = "Thisi is a test for mustche",
+                    IfAllowFeedback = true,
+                    HelfulPercent = 58,
+                    Items = new List<Item> {
+                        new Item { Name="test1"},
+                        new Item { Name="test2"}
+                    },
+                    Comp = new Complex
+                    {
+                        Child = new Child { Name = "Test Complex Type" }
+                    }
+                };
+            }
+        }
+
+        public class Item
+        {
+            public string Name { get; set; }
+        }
+
+        public class Complex
+        {
+            public Child Child { get; set; }
+        }
+        public class Child
+        {
+            public string Name { get; set; }
         }
     }
 }
